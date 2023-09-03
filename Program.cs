f071836b91a2f8a302485e86bde080bb8bcaec54
unknown <b.s.sattari@gmail.com>
2023-09-03 14:49:42 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Ters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Array adi;
+            Console.Write("Adınız :");
+            adi = Console.ReadLine().ToCharArray();
+            Array.Reverse(adi);           
+            
+            foreach (var karakter in adi)
+            {
+                Console.Write(karakter);
+            }
+
+            //  for(değişken tanımla ve başlangıç değeri ata; koşul belirt;arttırma azaltma yaptır){}
+           
+           
+           /* for (int i = adi.Length-1; i >=0 ; i--)
+            {
+               Console.Write(adi[i]);
+            }
+
+            */
+
+            
+        }
+    }
+}
